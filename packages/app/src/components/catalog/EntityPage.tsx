04a833a078128acf8f9a752f689e4e638ebb8bc4
Chris Impey <chris.impey@cloudmargin.com>
2022-12-09 17:16:42 +0000

    enable sonarqube on entity page

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -55,6 +55,7 @@ import {
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+import { EntitySonarQubeCard } from '@backstage/plugin-sonarqube';
 
 const techdocsContent = (
   <EntityTechdocsContent>
@@ -119,6 +120,9 @@ const overviewContent = (
     </Grid>
     <Grid item md={6} xs={12}>
       <EntityCatalogGraphCard variant="gridItem" height={400} />
+    </Grid>
+    <Grid item md={6}>
+        <EntitySonarQubeCard variant="gridItem" />
     </Grid>
 
     <Grid item md={4} xs={12}>
